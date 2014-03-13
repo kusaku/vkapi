74713184e0c019a1dd22e49c13661c76b846da75
Kirill Arkhipenko <kirill.arkhipenko@cloudmach.com>
2014-03-13 19:25:42 +0400

    added access token manipulations

--- vkapi.php
+++ vkapi.php
@@ -11,41 +11,174 @@ namespace VK;
  */
 class VKAPI {
 
-	var $api_secret;
-	var $app_id;
-	var $api_url;
+	private $api_secret;
+	private $app_id;
+	private $api_url;
+	private $api_version;
 
-	function __construct($app_id, $api_secret, $api_url = 'api.vk.com/method/') {
-		$this->app_id     = $app_id;
-		$this->api_secret = $api_secret;
-		if (!strstr($api_url, 'http://')) {
-			$api_url = 'http://' . $api_url;
+	public function __construct($cfg) {
+		// set default
+		$cfg += array(
+			'api_url'     => 'api.vk.com/method/',
+			'api_version' => '5.14',
+		);
+
+		$this->app_id     = $cfg['app_id'];
+		$this->api_secret = $cfg['api_secret'];
+		if (!strstr($cfg['api_url'], 'http://')) {
+			$cfg['api_url'] = 'https://' . $cfg['api_url'];
 		}
-		$this->api_url = $api_url;
+		$this->api_url     = $cfg['api_url'];
+		$this->api_version = $cfg['api_version'];
 	}
 
-	function exec($method, $params = false) {
-		if (!$params) {
-			$params = array();
+	private function get_access_token($set_access_token = null) {
+		static $access_token;
+		if ($set_access_token) {
+			$access_token = $set_access_token;
 		}
-		$params['api_id']    = $this->app_id;
-		$params['v']         = '5.11';
+		if (!$access_token) {
+			$params['client_id']     = $this->app_id;
+			$params['client_secret'] = $this->api_secret;
+			$params['v']             = $this->api_version;
+			$params['grant_type']    = 'client_credentials';
+
+			$query = 'https://oauth.vk.com/access_token' . '?' . http_build_query($params);
+
+			$context = stream_context_create(
+				array(
+					'http' =>
+						array(
+							'timeout'       => 5,
+							'ignore_errors' => true
+						)
+				));
+
+			$response = json_decode(file_get_contents($query, false, $context), true);
+
+			if (isset($response['error'])) {
+				throw new \Exception("{$response['error_description']} ({$response['error']})");
+			}
+
+			$access_token = $response['access_token'];
+		}
+
+		return $access_token;
+	}
+
+	public function set_access_token($access_token) {
+		return $this->get_access_token($access_token);
+	}
+
+	public function exec($method, $params = array(), $secure = false) {
+		$params['app_id']    = $this->app_id;
+		$params['v']         = $this->api_version;
 		$params['timestamp'] = time();
 		$params['format']    = 'json';
 		$params['random']    = rand(0, 10000);
 
-		ksort($params);
+		if ($secure) {
+			$params['client_secret'] = $this->api_secret;
+			$params['access_token']  = $this->get_access_token();
+		}
 
+		$params['sig'] = $this->sign($params);
+
+		$query = $this->api_url . $method . '?' . http_build_query($params);
+
+		$context = stream_context_create(
+			array(
+				'http' =>
+					array(
+						'timeout'       => 5,
+						'ignore_errors' => true
+					)
+			));
+
+		$response = json_decode(file_get_contents($query, false, $context), true);
+
+		if (isset($response['error'])) {
+			throw new \Exception("{$response['error']['error_msg']}", $response['error']['error_code']);
+		}
+
+		return $response;
+	}
+
+	public function sign($params) {
+		ksort($params);
 		$sig = '';
 		foreach ($params as $k => $v) {
 			$sig .= $k . '=' . $v;
 		}
 		$sig .= $this->api_secret;
-		$params['sig'] = md5($sig);
 
-		$query = $this->api_url . $method . '?' . http_build_query($params);
-		$res   = file_get_contents($query);
+		return md5($sig);
+	}
 
-		return json_decode($res, true);
+	public function validate($params) {
+		if (isset($params['sig'])) {
+			$sig = $params['sig'];
+			unset($params['sig']);
+
+			return $sig == $this->sign($params);
+		}
+
+		return false;
+	}
+
+	public function response($data) {
+		return json_encode(
+			array(
+				'response' => $data
+			));
+	}
+
+	public function error($error_code, $error_msg, $critical = false) {
+		return json_encode(
+			array(
+				'error' => array(
+					'error_code' => $error_code,
+					'error_msg'  => $error_msg,
+					'critical'   => $critical
+				)
+			));
+	}
+
+	public function upload($url, $files) {
+		$boundary = uniqid('!', true);
+		$header   = "Content-Type: multipart/form-data; boundary={$boundary}";
+		$content  = "--{$boundary}";
+
+		foreach ($files as $name => $path) {
+			$file_contents = file_get_contents($path);
+			$file_name     = basename($path);
+			$mime_type     = mime_content_type($path);
+			$content .= "\n";
+			$content .= "Content-Disposition: file; name=\"{$name}\"; filename=\"{$file_name}\"\n";
+			$content .= "Content-Type: {$mime_type}\n";
+			$content .= "Content-Transfer-Encoding: binary\n\n";
+			$content .= $file_contents;
+			$content .= "\n";
+			$content .= "--{$boundary}";
+		}
+
+		$content .= "--\n";
+
+		$context = stream_context_create(
+			array(
+				'http' => array(
+					'method'  => 'POST',
+					'header'  => $header,
+					'content' => $content,
+				)
+			));
+
+		$response = json_decode(file_get_contents($url, false, $context), true);
+
+		if (isset($response['error'])) {
+			throw new \Exception("{$response['error']['error_msg']}", $response['error']['error_code']);
+		}
+
+		return $response;
 	}
 }
